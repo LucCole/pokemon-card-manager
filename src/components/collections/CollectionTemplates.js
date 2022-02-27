d02849bd8ea6f41fb3a055786ee92fd6fb57b80d
Luc cole <luccoleb@yahoo.com>
2022-02-27 01:29:24 -0500

    Added react user components

--- src/components/collections/CollectionTemplates.js
+++ src/components/collections/CollectionTemplates.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getAllCollectionTemplates } from '../api';
+import { getAllCollectionTemplates } from '../../api';
 
-import { CardRow, CollectionTemplateInfo } from './';
+import { CardRow, CollectionTemplateInfo } from '..';
 
 
 const CollectionTemplates = () => {
